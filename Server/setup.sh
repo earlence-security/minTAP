378f9610a89513a88148632cd4fdd7ba6f1903da
Ubuntu <ubuntu@ip-172-31-42-237.us-east-2.compute.internal>
2022-03-22 09:36:39 +0000

    update docker setup

--- Server/setup.sh
+++ Server/setup.sh
@@ -3,7 +3,6 @@
 mkdir -p /mintap_log
 
 cd isolated_server
-npm install 
 node server > /mintap_log/node_log 2>&1 &
 
 cd ..
@@ -12,4 +11,4 @@ export FLASK_APP=service
 mkdir instance
 flask init-db
 
-flask run --host=0.0.0.0 > /mintap_log/flask_log 2>&1 &
+flask run --host=0.0.0.0 > /mintap_log/flask_log 2>&1 
